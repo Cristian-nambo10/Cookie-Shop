901dc47fc0e80943e73c5f2bfe28bce40ac0a6c2
Cristian Nambo <nambocristian10@gmail.com>
2022-05-30 14:31:42 -0500

    express server running

---
+++ server.js
@@ -0,0 +1,16 @@
+// Depemdencies
+const express = require('express');
+const app = express();
+
+
+
+
+
+
+
+
+
+
+// Listeners
+const PORT = 3000;
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
